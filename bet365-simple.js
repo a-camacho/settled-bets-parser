3b318a3d8afcecee73ea192aaf185d8c179880e3
André Camacho <andre@ebeasoftware.com>
2020-02-09 20:11:17 +0100

    more chagnes

--- bet365-simple.js
+++ bet365-simple.js
@@ -1,4 +1,20 @@
-function getBet365Bets( children ) {
+
+  function removeLineBreaks( string ) {
+    string = string.replace(/(\r\n|\n|\r)/gm, "");
+    return string;
+  }
+
+  function removeSpacesAtEnd( string ){
+    string = string.replace(/\s*$/,'');
+    return string;
+  }
+
+  function trimString( string ){
+    string = $.trim(string);
+    return string;
+  }
+
+  function getBet365Bets( children ) {
 
   my_bets = [];
 
@@ -19,6 +35,7 @@ function getBet365Bets( children ) {
 
         myMatches = [];
         matches = object.querySelectorAll(".bet-summary-detail-bet-selection");
+        
         $.each(( matches ), function( index ) {
 
           bet = matches[index].children[0].children[0].children[0].innerText;
@@ -27,7 +44,7 @@ function getBet365Bets( children ) {
           currentMatch = {bet: bet, odd: odd};
           myMatches.push(currentMatch);
 
-      });
+        });
 
       myBet.matches = myMatches;
 
@@ -42,6 +59,63 @@ function getBet365Bets( children ) {
     }
 
   });
+
+  return my_bets;
+
+}
+
+function getBet365DetailedBet( htmlBet, htmlTable ) {
+
+  my_bets = [];
+
+    myBet = {};
+
+      myBet.date = htmlBet.querySelectorAll(".bet-confirmation-header-datetime")[0].innerText.substr(0, 10);
+      myBet.time = htmlBet.querySelectorAll(".bet-confirmation-header-datetime")[0].innerText.substr(-8);
+
+      myMatches = [];
+      matches = htmlBet.querySelectorAll(".bet-confirmation-details")[0].children;
+
+      $.each(( matches ), function( index, object ) {
+
+        bet = object.querySelectorAll(".bet-confirmation-details-row-selectionname")[0].innerText;
+        bet = removeLineBreaks(bet);
+        
+        odd = object.querySelectorAll(".bet-confirmation-details-row-odds")[0].innerText;
+        odd = removeLineBreaks(trimString( odd ));
+
+        event = object.querySelectorAll(".bet-confirmation-details-row-eventname")[0].innerText;
+        event = removeLineBreaks(trimString( event ));
+
+        matchdetail = event.slice(0, -11);
+        matchdate = event.substring(event.length - 11);
+
+        bettype = object.querySelectorAll(".bet-confirmation-details-row-plbtdescription")[0].innerText;
+        bettype = removeLineBreaks(bettype);;
+
+        betresult = object.querySelectorAll(".bet-confirmation-details-row-status")[0].innerText;
+        betresult = removeLineBreaks(betresult);;
+
+        currentMatch = {bet: bet, odd: odd, matchdetail:matchdetail, matchdate:matchdate, bettype:bettype, betresult:betresult};
+        myMatches.push(currentMatch);
+
+      });
+
+      myBet.odd = htmlTable.querySelectorAll(".bet-breakdown-table-value")[2].innerText;
+      myBet.odd = removeLineBreaks(trimString( myBet.odd ));
+
+      myBet.stake = htmlTable.querySelectorAll(".bet-breakdown-table-value")[1].innerText;
+      myBet.stake = removeLineBreaks(trimString( myBet.stake ));
+
+      myBet.return = htmlTable.querySelectorAll(".bet-breakdown-table-value")[4].innerText;
+      myBet.return = removeLineBreaks(trimString( myBet.return ));
+
+      myBet.status = htmlTable.querySelectorAll(".bet-breakdown-table-value")[3].innerText;
+      myBet.status = removeLineBreaks(trimString( myBet.status ));
+
+      myBet.matches = myMatches;
+
+      my_bets.push(myBet);
 
   return my_bets;
 
---
+++ booklet.js
@@ -0,0 +1,7 @@
+// unminified
+
+javascript: void((function(){
+  console.log(document.getElementsByClassName("historyV3Iframe"));
+})());
+
+// minified at 

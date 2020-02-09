0bb67f988fd946af259f7be909e33c07a31d9d98
André Camacho <andre@ebeasoftware.com>
2020-02-09 21:00:39 +0100

    more changes

--- booklet.js
+++ booklet.js
@@ -1,7 +1,22 @@
-// unminified
-
 javascript: void((function(){
-  console.log(document.getElementsByClassName("historyV3Iframe"));
-})());
 
-// minified at 
+  var iframe = document.querySelector('#MembersHostFrame');
+  iframe = iframe.contentWindow.document;
+  iframe = iframe.querySelector('.historyV3Iframe');
+  iframe = iframe.contentWindow.document;
+  iframe = iframe.querySelector('.bet-confirmation-viewer');
+
+  var html = iframe.outerHTML;
+
+  var iframe2 = document.querySelector('#MembersHostFrame');
+  iframe2 = iframe2.contentWindow.document;
+  iframe2 = iframe2.querySelector('.historyV3Iframe');
+  iframe2 = iframe2.contentWindow.document;
+  iframe2 = iframe2.querySelector('.bet-breakdown-table');
+  var html2 = iframe2.outerHTML;
+
+  result = html + html2;
+
+  console.log(result);
+
+})());
